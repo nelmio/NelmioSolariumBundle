27881a997038380d6b8e688b0cf28689bb737054
Roland Franssen :) <ro0NL@users.noreply.github.com>
2025-02-25 08:10:18 +0100

    fix deprecation

--- src/DependencyInjection/NelmioSolariumExtension.php
+++ src/DependencyInjection/NelmioSolariumExtension.php
@@ -21,7 +21,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 /**
  * @author Igor Wiedler <igor@wiedler.ch>
